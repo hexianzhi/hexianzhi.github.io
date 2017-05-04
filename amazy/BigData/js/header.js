589a86637fe1cdac531fb209fcf18123928db61b
hexianzhi <hxz707950194@gmail.com>
2017-05-04 23:17:06 +0800

    Site updated: 2017-05-04 23:17:06

--- amazy/BigData/js/header.js
+++ amazy/BigData/js/header.js
@@ -2,12 +2,16 @@
  * Created by gedune on 2017/5/4.
  */
 
-$( document ).ready(function() {
+// $( document ).ready(function() {
+//
+// });
 
-
-});
-
-
+window.onload  = function() {
+    window.console.log("fuck");
+    var row_44 =document.getElementById("row_44");
+    row_44.style.display = "block";
+    row_44.style.bottom = "0px";
+}
 
 
 
